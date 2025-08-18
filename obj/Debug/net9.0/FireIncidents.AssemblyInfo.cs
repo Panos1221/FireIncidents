fd68ca698e84a311e814a86a6602c60f70bdd239
Panos1221 <pansavgr@gmail.com>
2025-08-18 16:08:07 +0300

    Misc Changes

--- obj/Debug/net9.0/FireIncidents.AssemblyInfo.cs
+++ obj/Debug/net9.0/FireIncidents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FireIncidents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6584bd6574d2eff4cb8dade57975b9b706d8fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a9eb3d65ef4207eee6e7cfd0b79867ec826e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("FireIncidents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FireIncidents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
