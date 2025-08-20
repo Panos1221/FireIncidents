8dc703a54bbc315c97eaeb1ffe2711048743d31e
Panos1221 <pansavgr@gmail.com>
2025-08-20 17:48:00 +0300

    SociableKIT Intergration to monitor official X accounts (X API is extremely expensive)

--- obj/Debug/net9.0/FireIncidents.AssemblyInfo.cs
+++ obj/Debug/net9.0/FireIncidents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FireIncidents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb6d2708dfae13edb1165a247f03034a3df76e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df10ada523bd5788a0c4dd55244a3e70fcb91a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("FireIncidents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FireIncidents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
